f624eadf8741a6c49daed3de041bab63a66625e8
Cameren Green <greencmaeren@gmail.com>
2024-09-14 16:22:58 -0500

    Create test.h

---
+++ test.h
@@ -0,0 +1,72 @@
+// C Program to write first program 
+#include <stdio.h> 
+
+typedef struct test_node test_node; 
+
+// Struct declared 
+struct test_node { 
+	test_node* next; 
+	void (*func)(void); 
+}; 
+
+extern void add_test(test_node* tn); 
+
+#define test_assert(b)									 \ 
+	do {												 \ 
+		if (b) {										 \ 
+			printf("PASSED\n");						 \ 
+			tests_passed++;							 \ 
+		}												 \ 
+		else {											 \ 
+			printf("FAILED\n");						 \ 
+			tests_failed++;							 \ 
+		}												 \ 
+	} while (0) 
+
+#define UNIT_TEST(f)									 \ 
+	static void f(void);								 \ 
+	static void __attribute__((constructor))			 \ 
+		__construct_##f(void)							 \ 
+	{													 \ 
+		static test_node tn;							 \ 
+		tn.next = NULL;								 \ 
+		tn.func = f;									 \ 
+		add_test(&tn);									 \ 
+	}													 \ 
+	static void __real_##f(void);						 \ 
+	static void f(void)								 \ 
+	{													 \ 
+		printf("%s... ", __func__);					 \ 
+		fflush(stdout);								 \ 
+		__real_##f();									 \ 
+	}													 \ 
+	static void __real_##f(void) 
+
+int tests_passed = 0, tests_failed = 0; 
+test_node* start = 0; 
+
+void add_test(test_node* tn) 
+{ 
+	test_node** current = &start; 
+	while (*current) 
+		current = &((*current)->next); 
+	*current = tn; 
+} 
+
+// Driver code 
+int main() 
+{ 
+	test_node* current = start; 
+	while (current) { 
+		current->func(); 
+		current = current->next; 
+	} 
+	int total_tests = tests_passed + tests_failed; 
+	if (total_tests == 0) 
+		printf("No tests found\n"); 
+	else
+		printf( 
+			"%i test case passed\n%i test case failed\n\n", 
+			tests_passed, tests_failed); 
+	return tests_failed > 0; 
+}
